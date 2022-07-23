824aa38c219071b13ad47c3746485037495ded7c
tarequem <moatartareque@gmail.com>
2022-07-23 00:16:48 -0400

    created API route for getting vote totals

---
+++ routes/apiRoutes/voteRoutes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+router.get('/votes', (req, res) => {
+    const sql = `SELECT * FROM votes`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+router.post('/vote', ({ body }, res) => {
+    // Data validation
+    const errors = inputCheck(body, 'voter_id', 'candidate_id');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+  
+    const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?,?)`;
+    const params = [body.voter_id, body.candidate_id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body,
+        changes: result.affectedRows
+      });
+    });
+});
+
+module.exports = router;
